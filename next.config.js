3da1b10495c557696741fe4f39490d80e8bbab06
Frank Desogus <riccardo.dibiagio88@gmail.com>
2024-04-19 10:22:19 +0200

    image  commit

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname:"mic-medusademo-tq9pqli72b-orientando-5-mediabucket-cexuvpvnsfar.s3.eu-central-1.amazonaws.com",
+      },
     ],
   },
 })
