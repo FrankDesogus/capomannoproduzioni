3066f7d557c85da7dbf83bddefcdbde7489f6643
Frank Desogus <riccardo.dibiagio88@gmail.com>
2024-04-19 09:01:09 +0200

    LavoriInCorso1 commit

--- src/modules/account/components/register/index.tsx
+++ src/modules/account/components/register/index.tsx
@@ -61,7 +61,7 @@ const Register = ({ setCurrentView }: Props) => {
         </div>
         <ErrorMessage error={message} data-testid="register-error" />
         <span className="text-center text-ui-fg-base text-small-regular mt-6">
-          By creating an account, you agree to Medusa Store&apos;s{" "}
+          By creating az account, you agree to Medusa Store&apos;s{" "}
           <LocalizedClientLink
             href="/content/privacy-policy"
             className="underline"
