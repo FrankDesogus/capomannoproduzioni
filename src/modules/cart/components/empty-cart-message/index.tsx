14d4f00828c7f01a224ee8e7e16de8a7f73df601
Frank Desogus <riccardo.dibiagio88@gmail.com>
2024-04-20 15:23:13 +0200

    cart

--- src/modules/cart/components/empty-cart-message/index.tsx
+++ src/modules/cart/components/empty-cart-message/index.tsx
@@ -12,11 +12,10 @@ const EmptyCartMessage = () => {
         Cart
       </Heading>
       <Text className="text-base-regular mt-4 mb-6 max-w-[32rem]">
-        You don&apos;t have anything in your cart. Let&apos;s change that, use
-        the link below to start browsing our products.
+        Non hai nulla nel carrello. Cambiamo la situazione,utilizza il link qui sotto per iniziare a sfogliare i nostri prodotti
       </Text>
       <div>
-        <InteractiveLink href="/store">Explore products</InteractiveLink>
+        <InteractiveLink href="/store">Esplora i prodotti</InteractiveLink>
       </div>
     </div>
   )
