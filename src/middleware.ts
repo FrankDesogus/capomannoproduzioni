09dc2392a2f752ccf482ae598c79aa11b79b32b4
FrankDesogus <riccardo.dibiagio88@gmail.com>
2024-06-14 02:51:52 +0200

    logo

--- src/middleware.ts
+++ src/middleware.ts
@@ -138,4 +138,4 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|orientando.ico).*)']}
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']}
