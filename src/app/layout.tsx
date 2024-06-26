4b65d9dfc535475b8f155128f02c5cdc0f846655
FrankDesogus <riccardo.dibiagio88@gmail.com>
2024-06-26 09:27:34 +0200

    fix missin gdpr1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+      <head></head>
       <body>
         <main className="relative">{props.children}</main>
       </body>
